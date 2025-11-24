49205caa985280c7190f38b09496130dc43f75cf
Monster0506 <tjraklovits@gmail.com>
2025-11-24 00:02:48 -0500

    Comments for final version added

--- lesson_utils/final/app.py
+++ lesson_utils/final/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request  # basic imports
 
 
-app = Flask(__name__)
+app = Flask(__name__)  # flask boilerplate
 
+
+## Our list of colors
 
 colors = {
     0: "#ff00ff",
@@ -12,24 +14,35 @@ colors = {
 }
 
 
-@app.route("/")
-def main():
-    return render_template("index.html", colors=colors)
+@app.route("/")  # on the / route
+def main():  # do this function
+    return render_template(
+        "index.html",  # return the index.html file in templates/
+        colors=colors,  # with all instances of {{colors}} replaced with the actual colors dictionary
+    )
 
 
-@app.route("/picker")
-def picker():
-    square = int(request.args.get("square", 0))
-    return render_template("picker.html", square=square, colors=colors)
+@app.route("/picker")  # on the picker route
+def picker():  # do this function
+    square = int(
+        request.args.get("square", 0)
+    )  # get X as an integer from /picker?square=X
+    return render_template(
+        "picker.html", square=square, colors=colors
+    )  # same thing as before, with square passed, too
 
 
-@app.route("/update_color", methods=["POST"])
+@app.route(
+    "/update_color", methods=["POST"]
+)  # ONLY on submissions from the form at the update_color route
 def update():
-    square = int(request.form.get("square"))
-    color = request.form.get("color")
-    colors[square] = color
+    square = int(
+        request.form.get("square")
+    )  # get the form response of the square number
+    color = request.form.get("color")  # get the form response of the color
+    colors[square] = color  # change the dictionary value to the new color
 
-    return redirect("/")
+    return redirect("/")  # go back to the home page
 
 
 if __name__ == "__main__":
